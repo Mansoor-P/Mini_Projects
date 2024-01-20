3e0e6548908b0e70f0bb11e053463ddb5043e1a6
mansoor-P <munaf6007@gmail.com>
2024-01-20 11:57:07 +0530

    todo app

--- React-Projects/todo-app/src/App.jsx
+++ React-Projects/todo-app/src/App.jsx
@@ -6,23 +6,29 @@ import TodoItems from "./components/TodoItems";
 import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 function APP() {
-
   const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
-    // console.log(`new item  ${itemName} added Date : ${itemDueDate}`);
     const newTodoItems = [
       ...todoItems,
       { name: itemName, dueDate: itemDueDate },
     ];
     setTodoItems(newTodoItems);
   };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
   return (
     <center className="todo-content">
       <AppName />
       <AddTodo onNewItem={handleNewItem} />
-      {todoItems.length ===0&&<WelcomeMessage ></WelcomeMessage>}
-      <TodoItems todoItems={todoItems}></TodoItems>
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
     </center>
   );
 }
--- React-Projects/todo-app/src/components/TodoItem.jsx
+++ React-Projects/todo-app/src/components/TodoItem.jsx
@@ -1,13 +1,19 @@
-function TodoItem({todoName,todoDate}){
-return <div className="container ms-row">
-  <div className="row">
-<div className="col-6">{todoName}</div>
-<div className="col-4">{todoDate}</div>
-<div className="col-2">
-  <button className="btn btn-danger ms-button">Delete</button>
-</div>
-</div>
-</div>
-
+function TodoItem({ todoName, todoDate, onDeleteClick }) {
+  return (
+    <div className="container ms-row">
+      <div className="row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button
+            className="btn btn-danger ms-button"
+            onClick={() => onDeleteClick(todoName)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default TodoItem;
