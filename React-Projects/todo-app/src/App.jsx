0cf550c5726a03f08e268d272bdbfb71172b6eea
mansoor-P <munaf6007@gmail.com>
2024-01-21 08:06:59 +0530

    todo app

--- React-Projects/todo-app/src/App.jsx
+++ React-Projects/todo-app/src/App.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import "./App.css";
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+import { TodoItemsContext } from "./store/todo-items-store";
 
 function APP() {
- 
+  const [todoItems, setTodoItems] = useState([]);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+
+    // same functionality
+
+    // setTodoItems((currValue) => {
+    //   const newTodoItems = [
+    //     ...currValue,
+    //     { name: itemName, dueDate: itemDueDate },
+    //   ];
+    //   return newTodoItems;
+    // });
+  };
+
+  const deleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
   return (
-    <>
-    social-media
-    </>
+    <TodoItemsContext.Provider value={{
+      todoItems,
+      addNewItem,
+      deleteItem,
+    }}>
+      <center className="todo-content">
+        <AppName />
+        <AddTodo />
+        <WelcomeMessage></WelcomeMessage>
+        <TodoItems></TodoItems>
+      </center>
+    </TodoItemsContext.Provider>
   );
 }
 export default APP;
